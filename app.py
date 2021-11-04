ee1ab7f591e90b6a719afb8c5269b19a19ffb7e2
AlexandrGergiev <gergieverty3@gmail.com>
2021-11-04 10:45:31 +0200

    .

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+with open("data.json") as file:
+    data = json.load(file)
+
+
+@app.route("/")
+def my_foo():
+    return render_template("main.jinja", schedule=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
